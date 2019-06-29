69b7cd6803eaa351d4f2eebf96f39921df04e39b
Matthew Lau <matthew@Matthews-MacBook-Pro.local>
2019-06-29 18:48:55 +0800

    added home html

--- slackify/urls.py
+++ slackify/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import message.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', message.views.home, name='home'),
 ]
