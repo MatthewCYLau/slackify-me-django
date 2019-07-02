786f90d63860f913559dad30ffe5879f08243133
Matthew Lau <matthew@Matthews-MacBook-Pro.local>
2019-07-02 09:08:47 +0800

    s

--- message/form.py
+++ message/form.py
@@ -11,7 +11,7 @@ class InputMessageForm(forms.ModelForm):
 
     name = forms.CharField(label='Your name', widget=forms.TextInput(attrs={"placeholder": "Enter name here"}))
     channel = forms.CharField(label='Slack channel', widget=forms.Select(choices=CHANNEL_CHOICES))
-    body = forms.CharField(label='Your message', widget=forms.TextInput(attrs={'class':'form-control', "placeholder": "Enter slack message body here"}))
+    body = forms.CharField(label='Your message', widget=forms.TextInput(attrs={'class': 'form-control', "placeholder": "Enter slack message body here"}))
 
     class Meta:
         model = InputMessage
